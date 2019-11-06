a29f1cfacfd1f5c576aa703247567d094a384945
qadratic <ripudaman.kaushik@yahoo.in>
2019-11-06 17:50:13 +0530

    added permissions and advertising done

--- App.js
+++ App.js
@@ -1,114 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React,{useState} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+	Button,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Nearby2,{startAdvertising} from 'react-native-nearby2';
 
-const App: () => React$Node = () => {
+const App = () => {
+	// let xx=()=>{
+	// 	Nearby2.funcWithPromise(true)
+	// 	.then(x=>console.log(x))
+	// 	.catch(x=>console.log(x));
+	// }
+	// xx();
+	const [advertisingMessage,setAdvertisingMessage]=useState("");
+	let startAdvertisingNow=()=>{
+		Nearby2.startAdvertising()
+		.then(()=>{
+			setAdvertisingMessage("advertising!");
+		})
+		.catch((e)=>{
+			setAdvertisingMessage("failed to advertise!"+e);
+		})
+
+	}
+	let stopAdvertisingNow=()=>{
+		Nearby2.stopAdvertising()
+		.then(()=>{
+			setAdvertisingMessage("stopped advertising!");
+		})
+		.catch((e)=>{
+			setAdvertisingMessage("failed to stop advertise!"+e);
+		})
+
+	}
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+	<Button title="start advertising" onPress={()=>{startAdvertisingNow()}}></Button>
+	<Button title="stop advertising" onPress={()=>{stopAdvertisingNow()}}></Button>
+      <Text>{advertisingMessage}</Text>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
