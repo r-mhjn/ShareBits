4d2fae8c0c201ddc136db97e53f5ffc7503db6bf
qadratic <ripudaman.kaushik@yahoo.in>
2019-11-08 13:58:05 +0530

    setup

--- backend/routes/file.js
+++ backend/routes/file.js
@@ -1,5 +1,12 @@
 const router=require('express').Router();
+const multer=require('multer');
 
-router.route('/upload').post();
+const upload=multer({dest:'uploads/'});
+
+router.route('/upload').post(upload.single('hell'),(req,res)=>{
+	res.json('ok');
+});
+
+router.route('/download').post();
 
 module.exports=router;
